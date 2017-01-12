718469f7a7ca8a105474a1117f73a2e65ce3f1b1
pmanoj <pmanoj@tracfone.com>
2017-01-12 16:38:13 -0500

    Fixed defect in while loop

--- src/main/java/com/cognizant/sqs/SimpleQueueServiceSample.java
+++ src/main/java/com/cognizant/sqs/SimpleQueueServiceSample.java
@@ -29,7 +29,7 @@ public class SimpleQueueServiceSample {
 			System.out.println("Messages size : " + messages.size());
 			if (messages == null || messages.size() == 0) {
 				try {
-					Thread.sleep(1000);
+					Thread.sleep(1000);// comment
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
